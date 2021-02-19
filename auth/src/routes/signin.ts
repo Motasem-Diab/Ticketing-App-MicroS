ef03076ee3f9d2aad937fb1702ce37599784169b
Motasem-Diab <diabmotasem@gmail.com>
2021-02-19 23:47:13 +0200

    signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express' ;
+import express, {Request, Response} from 'express' ;
+import { body, validationResult } from 'express-validator';
+import { RequesValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req,res)=>{
-    res.send('Hi there ');
-});
+router.post('/api/users/signin',
+    [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid .,'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('Must supply a Password')
+    ],
+    (req: Request, res: Response)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            throw new RequesValidationError(errors.array());
+        }
+        const { email, password } = req.body;
+    });
 
 export { router as signinRouter };
