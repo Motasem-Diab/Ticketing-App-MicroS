622f1b8b5f404e20146e609593e7b9d92f93afbf
Motasem-Diab <diabmotasem@gmail.com>
2021-02-27 18:33:56 +0200

    transfer mongo_uri to env variable

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,11 +7,14 @@ import { app } from './app';
 const start = async () =>{
 
     if(!process.env.JWT_KEY){
-        throw new Error('JWT_KEY not defined !!!');
+        throw new Error('JWT_KEY is not defined !!!');
+    }
+    if(!process.env.MONGO_URI){
+        throw new Error('MONGO_URI is not defined !!!');
     }
 
     try{
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
         useUnifiedTopology: true, 
         useNewUrlParser: true,
         useCreateIndex: true
