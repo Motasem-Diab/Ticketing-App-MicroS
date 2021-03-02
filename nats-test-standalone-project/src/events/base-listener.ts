bf508a0b3497b4363f5314dc4f1c150b092955cb
Motasem-Diab <diabmotasem@gmail.com>
2021-03-03 01:49:48 +0200

    Listener as a Class

---
+++ nats-test-standalone-project/src/events/base-listener.ts
@@ -0,0 +1,52 @@
+import { Message, Stan } from 'node-nats-streaming';
+
+
+export abstract class Listener {
+    abstract subject: string;
+    abstract queueGroupName: string;
+    abstract onMessage(data: any, msg:Message): void ;
+    private client: Stan ;
+    protected ackWait = 5 * 1000 ; // 5s
+
+    // Connected client
+    constructor(client: Stan){
+        this.client = client
+    }
+
+    subscriptionOptions(){
+        return this.client
+            .subscriptionOptions()
+            .setDeliverAllAvailable()    // Resend all the missed event when the service is down
+            .setManualAckMode(true)         // if the Event lossed (information that event handel "add the ..." not saved in DB)
+                                            // Resend the event after 30s if no Ack
+            .setAckWait(this.ackWait)
+            .setDurableName(this.queueGroupName); // To resend only the events that not proccessed
+                        // The Queue group is important here to not delete the Durable when restart the service
+    }
+
+    listen(){
+        //                                      channel      Queue group (optional)  optional
+        const subscription = this.client.subscribe(
+            this.subject,
+            this.queueGroupName,
+            this.subscriptionOptions()
+        );
+
+        subscription.on('message', (msg: Message) => {
+            console.log(
+                `Message #${msg.getSequence()} received: ${this.subject} / ${this.queueGroupName}`
+            );
+
+            const parsedData = this.parseMessage(msg);
+            this.onMessage(parsedData, msg);
+        });
+    }
+
+    parseMessage(msg: Message){
+        const data = msg.getData();
+
+        return typeof data === 'string'
+            ? JSON.parse(data)
+            : JSON.parse(data.toString('utf8'));  // Just in case
+    }
+}
