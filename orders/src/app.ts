c1a1ca1f9fcf74b2993be8d8b883fa4a6c4253d9
Motasem-Diab <diabmotasem@gmail.com>
2021-03-06 02:39:37 +0200

    orders service initialization

---
+++ orders/src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler } from '@e-commerce-social-media/common';
+import { NotFoundError } from '@e-commerce-social-media/common';
+
+import { currentUser } from '@e-commerce-social-media/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/showTicket';
+import { showAllTicketsRouter } from './routes/showAllTickets';
+import { updateTicketRouter } from './routes/updateTicket'
+
+const app = express();
+app.set('trust proxy', true);  // to use https below
+
+app.use( json() ) ;
+
+app.use(
+    cookieSession({
+        signed: false,   // not encrypted
+        secure: process.env.NODE_ENV !== 'test'   // only with https connection
+    })
+)
+
+app.use(currentUser);
+
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(showAllTicketsRouter);
+app.use(updateTicketRouter);
+
+
+
+app.all('*', async()=>{
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app } ;
---
+++ orders/src/index.ts
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+import { natsWrapper } from './nats-wrapper';
+
+
+const start = async () =>{
+
+    if(!process.env.JWT_KEY){
+        throw new Error('JWT_KEY is not defined !!!');
+    }
+    if(!process.env.MONGO_URI){
+        throw new Error('MONGO_URI is not defined !!!');
+    }
+    if(!process.env.NATS_URL){
+        throw new Error('NATS_URL is not defined !!!');
+    }
+    if(!process.env.NATS_CLUSTER_ID){
+        throw new Error('NATS_CLUSTER_ID is not defined !!!');
+    }
+    if(!process.env.NATS_CLIENT_ID){
+        throw new Error('NATS_CLIENT_ID is not defined !!!');
+    }
+
+    try{                
+        //       ticketing is like what we put in infra file and the url also
+        //                                          unique id
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed');
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+
+        await mongoose.connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true, 
+        useNewUrlParser: true,
+        useCreateIndex: true
+        });
+        console.log('Connected to auth DB ..,.');
+    }
+    catch (err){
+        console.log('Cant connect to auth DB .. !!!', err)
+    }
+
+    app.listen(3000, ()=>{ 
+        console.log('Listening to port 3000');
+    });
+};
+
+start();
+
+
+
+// app.listen(3000, ()=>{ console.log('Listening to port 3000');} );
