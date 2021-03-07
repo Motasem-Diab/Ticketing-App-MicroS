d17799b98ab6eb9663a2b46df4d118892b2559a8
Motasem-Diab <diabmotasem@gmail.com>
2021-03-07 02:30:25 +0200

    show a specific order with test

--- orders/src/routes/showOrder.ts
+++ orders/src/routes/showOrder.ts
@@ -1,12 +1,27 @@
 import express, {Request, Response} from 'express' ;
-import { requireAuth, validateRequest } from '@e-commerce-social-media/common';
-import { body } from 'express-validator';
+import { BadRequesError, NotAuthorizedError, NotFoundError } from '@e-commerce-social-media/common';
+import { Order } from '../../models/order'
+import mongoose  from 'mongoose';
 
 
 const router = express.Router();
 
 router.get('/api/orders/:orderId', async(req:Request, res:Response) => {
-    res.send({});
+    const isValidId = mongoose.Types.ObjectId.isValid(req.params.orderId) ;
+    if(!isValidId){
+        throw new BadRequesError('Invalid give order id');
+    }
+
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+    if(!order){
+        throw new NotFoundError();
+    }
+
+    if(order.userId !== req.currentUser!.id){
+        throw new NotAuthorizedError();
+    }
+
+    res.send(order);
 });
 
 export { router as showOrderRouter };
