f3ab2b627fb669557e1a31fff16f25ff28a45609
Motasem-Diab <diabmotasem@gmail.com>
2021-03-07 02:17:06 +0200

    show all orders of the signed user with test

--- orders/src/routes/showAllOrders.ts
+++ orders/src/routes/showAllOrders.ts
@@ -1,12 +1,17 @@
 import express, {Request, Response} from 'express' ;
 import { requireAuth, validateRequest } from '@e-commerce-social-media/common';
-import { body } from 'express-validator';
+import { Order } from '../../models/order';
 
 
 const router = express.Router();
 
-router.get('/api/orders', async(req:Request, res:Response) => {
-    res.send({});
+router.get('/api/orders', requireAuth, async(req:Request, res:Response) => {
+    
+    const orders = await Order.find({
+        userId: req.currentUser!.id
+    }).populate('ticket');
+
+    res.send(orders);
 });
 
 export { router as showAllOrdersRouter };
