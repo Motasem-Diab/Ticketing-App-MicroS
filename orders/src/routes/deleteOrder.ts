9eda840815f6ac2dd2a6882167678ee568a7cc23
Motasem-Diab <diabmotasem@gmail.com>
2021-03-07 02:50:15 +0200

    delete order with test

--- orders/src/routes/deleteOrder.ts
+++ orders/src/routes/deleteOrder.ts
@@ -1,12 +1,27 @@
 import express, {Request, Response} from 'express' ;
-import { requireAuth, validateRequest } from '@e-commerce-social-media/common';
-import { body } from 'express-validator';
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@e-commerce-social-media/common';
+import { Order, OrderStatus } from '../../models/order'
 
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async(req:Request, res:Response) => {
-    res.send({});
+router.delete('/api/orders/:orderId', requireAuth, async(req:Request, res:Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+    if(!order){
+        throw new NotFoundError();
+    }
+    if(order.userId !== req.currentUser!.id){
+        throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // Publishig an event saying this orderd is cancelled
+
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
