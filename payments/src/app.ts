b8fe5cd91c785b3a070a66e681d00f71703c1bd0
Motasem-Diab <diabmotasem@gmail.com>
2021-03-19 16:19:13 +0200

    init payment service

---
+++ payments/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler } from '@e-commerce-social-media/common';
+import { NotFoundError } from '@e-commerce-social-media/common';
+
+import { currentUser } from '@e-commerce-social-media/common';
+
+
+const app = express();
+app.set('trust proxy', true);  // to use https below
+
+app.use( json() ) ;
+
+app.use(
+    cookieSession({
+        signed: false,   // not encrypted
+        secure: process.env.NODE_ENV !== 'test'   // only with https connection
+    })
+)
+
+app.use(currentUser);
+
+
+
+
+app.all('*', async()=>{
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app } ;
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,77 @@
+
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+
+import jwt from 'jsonwebtoken';
+
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin() : string[]
+        }
+    }
+}
+
+
+jest.mock('../nats-wrapper');  // Make a fake NATS client to pass the tests
+
+
+let mongo: any ;
+beforeAll( async ()=> {
+
+    process.env.JWT_KEY = 'random';
+
+    mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+});
+
+beforeEach( async () => {
+
+    jest.clearAllMocks(); // Testing mock event publisher
+
+    const collections = await mongoose.connection.db.collections() ;
+
+    for( let collection of collections){
+        await collection.deleteMany( {} );
+    }
+});
+
+afterAll( async () => {
+    await mongo.stop();
+    await mongoose.connection.close();
+});
+
+
+// Or make it in a seperate file and export it as regular
+global.signin =  () => {
+    // We will made a fake jwt
+    // Build a JWT payload. {id, email}
+
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(), // only to make it random
+        email: 'test@test.com'
+    }
+
+    // Create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!) ;
+
+    // Build the session object {JWT: MY_JWT}
+    const session = { jwt: token};
+
+    // Turn that session into json
+    const sessionJSON = JSON.stringify(session);
+
+    // Take json and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    // Return a string thats the cookie with the encoded data
+    return [`express:sess=${base64}`];
+}
