7de9373da7fd88afe3260ec3e96dac00f4fc9e7d
Motasem-Diab <diabmotasem@gmail.com>
2021-03-24 01:37:14 +0200

    payment created publisher and usage

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -6,6 +6,8 @@ import { Order } from '../models/order';
 import { stripe } from '../stripe';
 
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 
 const router = express.Router();
@@ -42,7 +44,13 @@ async (req: Request, res: Response) => {
     });
     await payment.save();
 
-    res.status(201).send({ success:true });
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+        id: payment.id,
+        orderId: payment.orderId,
+        stripId: payment.stripeId
+    });
+
+    res.status(201).send(payment);
 
 });
 
